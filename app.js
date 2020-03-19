ac09b07a0ed311bde066a26b33ee5ddff3723a06
cham_why <53082717+sjufkaus2@users.noreply.github.com>
2020-03-19 22:19:10 +0900

    1.0

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
